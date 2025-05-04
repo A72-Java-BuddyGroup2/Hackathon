b61d6d5aaa80e71a21e8ed34864d3e18c92e6151
vshopov996 <vshopov96@gmail.com>
2025-05-04 20:54:38 +0300

    added 'contains' and 'copyFrom' methods

--- Skeleton/src/com/company/core/arrays/ArrayHelpers.java
+++ Skeleton/src/com/company/core/arrays/ArrayHelpers.java
@@ -15,17 +15,45 @@ public class ArrayHelpers {
         return new int[1];
     }
 
+    /**
+     * Checks if source contains element
+     * @param source int[] - The array to check in
+     * @param element int - the elements to check for
+     * @return boolean - 'true' if source contains the element, otherwise 'false'
+     *
+     * @author Vasil Shopov
+     */
     public static boolean contains(int[] source, int element) {
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == element) {
+                return true;
+            }
+        }
         return false;
     }
+
 
     public static void copy(int[] sourceArray, int[] destinationArray, int count) {
 
     }
 
+    /**
+     * Copies elements from sourceArray, starting from sourceStartIndex into destinationArray, starting from
+     * destStartIndex, taking count elements.
+     * @param sourceArray int[] - The array to copy from
+     * @param sourceStartIndex int - The starting index in sourceArray
+     * @param destinationArray int[] - The array to copy to
+     * @param destStartIndex int - The starting index in destinationArray
+     * @param count int - The number of elements to copy
+     * @return - void
+     *
+     * @author Vasil Shopov
+     */
     public static void copyFrom(int[] sourceArray, int sourceStartIndex,
                                 int[] destinationArray, int destStartIndex, int count) {
-
+        for (int i = 0; i < count; i++) {
+            destinationArray[destStartIndex + i] = sourceArray[sourceStartIndex + i];
+        }
     }
 
     public static void fill(int[] source, int element) {
